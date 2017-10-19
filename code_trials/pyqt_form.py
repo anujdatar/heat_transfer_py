78ef40eceb2b7c1d60ec71c2dd13a04ef4c538a7
Anuj Datar <anuj.datar@gmail.com>
2017-10-19 01:41:16 -0400

    moved files to SD card

--- code_trials/pyqt_form.py
+++ code_trials/pyqt_form.py
@@ -1,17 +1,19 @@
 # coding=utf-8
 
 import sys
-from PyQt5 import QtWidgets as Qtw
-from PyQt5 import QtGui as Qtg
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QAction)
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
 
 
-class SampleMain(Qtw.QMainWindow):
+class SampleMain(QMainWindow):
 
     def __init__(self, width=640, height=480):
         super().__init__()
 
-        self.setWindowTitle('Sample Window')
         self.setGeometry(50, 50, width, height)
+        self.setWindowTitle('Sample Window')
+        self.setWindowIcon(QIcon('web.png'))
         self.statusBar().showMessage('Welcome')
 
         GenerateMenu(self)
@@ -19,7 +21,7 @@ class SampleMain(Qtw.QMainWindow):
         self.show()
 
 
-class SampleForm(Qtw.QWidget):
+class SampleForm(QWidget):
 
     def __init__(self, parent=None):
         super().__init__()
@@ -37,23 +39,24 @@ class GenerateMenu:
         help_menu = menu_top.addMenu('Help')
 
         # %%%%%%% add/edit material button
-        option_new_material = Qtw.QAction('Add/Edit Material', self.parent)
+        option_new_material = QAction('Add/Edit Material', self.parent)
         option_new_material.setStatusTip('Add or edit a material in database')
         edit_menu.addAction(option_new_material)
 
         # %%%%%%%%%%%%%% exit button
-        option_exit = Qtw.QAction('Exit', self.parent)
+        __ico_file = '/opt/google/chrome/product_logo_64.png'
+        option_exit = QAction(QIcon(__ico_file), 'Exit', self.parent)
         option_exit.setShortcut('Ctrl+Q')
         option_exit.setStatusTip('Exit application')
-        option_exit.triggered.connect(self.parent.close)
+        option_exit.triggered.connect(QCoreApplication.instance().quit)
         file_menu.addAction(option_exit)
 
         # %%%%%%%%%%%% about button
-        option_about = Qtw.QAction('About', self.parent)
+        option_about = QAction('About', self.parent)
         help_menu.addAction(option_about)
 
 
 if __name__ == '__main__':
-    app = Qtw.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     ex = SampleMain()
     sys.exit(app.exec_())
