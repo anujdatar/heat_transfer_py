bac305c75ee39cab154b731babe0b3aa515b5518
Anuj Datar <anuj.datar@gmail.com>
2017-10-24 23:21:35 -0400

    ...

--- code_trials/pyqt_form.py
+++ code_trials/pyqt_form.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 
 import sys
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QAction)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QAction,
+                             QLabel)
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore
 from PyQt5.QtCore import QCoreApplication
 
 
@@ -17,6 +19,7 @@ class SampleMain(QMainWindow):
         self.statusBar().showMessage('Welcome')
 
         GenerateMenu(self)
+        SampleForm(self)
 
         self.show()
 
@@ -28,32 +31,35 @@ class SampleForm(QWidget):
 
         self.parent = parent
 
+        self.label_density = QLabel('Density', self.parent)
+        self.label_density.move(100, 100)
+
 
 class GenerateMenu:
     def __init__(self, parent):
         self.parent = parent
 
-        menu_top = self.parent.menuBar()
-        file_menu = menu_top.addMenu('File')
-        edit_menu = menu_top.addMenu('Edit')
-        help_menu = menu_top.addMenu('Help')
+        self.menu_top = self.parent.menuBar()
+        self.file_menu = self.menu_top.addMenu('File')
+        self.edit_menu = self.menu_top.addMenu('Edit')
+        self.help_menu = self.menu_top.addMenu('Help')
 
         # %%%%%%% add/edit material button
-        option_new_material = QAction('Add/Edit Material', self.parent)
-        option_new_material.setStatusTip('Add or edit a material in database')
-        edit_menu.addAction(option_new_material)
+        self.option_new_material = QAction('Add/Edit Material', self.parent)
+        self.option_new_material.setStatusTip('Add or edit a material in database')
+        self.edit_menu.addAction(self.option_new_material)
 
         # %%%%%%%%%%%%%% exit button
         __ico_file = '/opt/google/chrome/product_logo_64.png'
-        option_exit = QAction(QIcon(__ico_file), 'Exit', self.parent)
-        option_exit.setShortcut('Ctrl+Q')
-        option_exit.setStatusTip('Exit application')
-        option_exit.triggered.connect(QCoreApplication.instance().quit)
-        file_menu.addAction(option_exit)
+        self.option_exit = QAction(QIcon('exit.png'), 'Exit', self.parent)
+        self.option_exit.setShortcut('Ctrl+Q')
+        self.option_exit.setStatusTip('Exit application')
+        self.option_exit.triggered.connect(QCoreApplication.instance().quit)
+        self.file_menu.addAction(self.option_exit)
 
         # %%%%%%%%%%%% about button
-        option_about = QAction('About', self.parent)
-        help_menu.addAction(option_about)
+        self.option_about = QAction('About', self.parent)
+        self.help_menu.addAction(self.option_about)
 
 
 if __name__ == '__main__':
